e1c5223b19e9f38b2faf6b075a84e9983b5460e0
kstagaman <kstagaman@gmail.com>
2022-09-30 14:20:50 -0700

    fixed issue with bad gitignore line

---
+++ R/mothur_NAST_align.R
@@ -0,0 +1,36 @@
+#' @name mothur.NAST.align
+#' @title NAST Alignment with `mothur`
+#' @description Uses the `align.seqs` function from `mothur` (requires install: https://mothur.org/wiki/installation/)
+#' @param fasta required; the path and name of the FASTA file containing the sequences to be aligned
+#' @param template.file required; the path and name of the alignment file to be used. See https://mothur.org/wiki/alignment_database/ to download a database of your choice.
+#' @param mothur.path character; path to `mothur` executable if it is not in your system PATH variable. Default is NULL.
+#' @param output.dir character; path to output directory. Default is `getwd()`.
+#' @param ncores integer; the number of cores for `mothur`.
+#' @seealso \code{\link{generate.tool.command}}, \code{\link{system}}
+#' @export
+
+mothur.NAST.align <- function(fasta, template.file, mothur.path = NULL, output.dir = getwd(), ncores = 1) {
+  if (is.null(mothur.path)) {
+    sys.mothur.path <- Sys.which("mothur")
+    if (!str_detect(sys.mothur.path, "mothur")) {
+      rlang::abort(
+        "Argument `mothur.path' is NULL, but the mothur executable is not in your PATH variable. Please add mothur to PATH or supply the path to the executable with `mothur.path'"
+      )
+    } else {
+      mothur.path <- sys.mothur.path[1]
+    }
+  }
+  cmd <- generate.tool.command(
+    tool.path = mothur.path,
+    function.name = "align.seqs",
+    tool.syntax = "mothur",
+    fasta = fasta,
+    reference = template.file,
+    flip = "t",
+    keepdots = "t",
+    processors = ncores,
+    outputdir = output.dir
+  )
+  cat(paste("Executing command:", cmd), sep = "\n")
+  system(cmd)
+}
